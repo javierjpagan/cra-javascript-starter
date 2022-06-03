fd740abc5e5af6e159344ec5be3d90620e33e24e
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2022-06-02 23:11:57 -0400

    Updated code for NFT Exclusiveness

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
+import { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  // get address of user & allow them to connect with metamask
   const address = useAddress();
   const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+
+  // add nft Drop contract
+  const nftDrop = useNFTDrop("0x66463b3C1EBf08b9dE889BCc0A5cbf29dc0e2B7a");
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  // function to claim NFT
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await nftDrop.claim(1);
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error("Failed to mint NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
+  //if there isn't a wallet connected, display our connect MetaMask button
+  if (!address) {
+    return (
+      <>
+        <h1>Welcome to the Cookie Club</h1>
+        <button className="btn" onClick={connectWithMetamask}>
+          Connect MetaMask
+        </button>
+      </>
+    );
+  }
+
+  // if the user is connected and has an NFT from the drop, display text
+  if (hasClaimedNFT) {
+    return <h2>Congratulations! You have a Cookie NFT! 🍪</h2>;
+  }
+
+  // helper function to truncate the address so it displays in a nice format
+  function truncateAddress(address) {
+    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  }
+
+  //if the user does not have an NFT, show their address and mint an NFT button
   return (
-    <div>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
-    </div>
+    <>
+      <p className="address">
+        There are no Cookie NFTs held by:{" "}
+        <span className="value">{truncateAddress(address)}</span>
+      </p>
+      <button className="btn mint" disabled={isClaiming} onClick={mintNft}>
+        Mint NFT
+      </button>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Rinkeby;
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
       <App />
     </ThirdwebProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
